70654e8135468cf3509df3b2928f46ed792888c2
fayzzz <mustafo.fayz@yandex.ru>
2021-01-31 04:42:52 +0500

    Made base json to ui convseration and need to fix several bugs.

--- src/src/scenes/App/index.tsx
+++ src/src/scenes/App/index.tsx
@@ -1,7 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './styles.scss';
 
+enum FieldType {
+    INPUT = 'INPUT',
+    OUTPUT = 'OUTPUT'
+}
+
 export const App = () => {
-    return <h1>Hello from my application</h1>
+    const [input, setInput] = useState('');
+    const [output, setOutput] = useState('');
+
+    const is_valid_json = (json: string) => {
+        try {
+            return [true, JSON.parse(json)];
+        } catch (e) {
+            return [false, e];
+        }
+    }
+
+    const change_text = (e: React.ChangeEvent<HTMLTextAreaElement>, type: FieldType) => {
+        const { value } = e.target;
+
+        try {
+            switch (type) {
+                case FieldType.INPUT:
+                    setInput(value);
+                    break;
+                case FieldType.OUTPUT:
+                    setOutput(value);
+                    break;
+                default:
+                    throw new Error('Invalid type for field!')
+                }
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    const generate_tree = (tree: any, key: string, level: string[] = []) => {
+        let current_tree = '';
+
+        Object.keys(tree).forEach((key) => {
+            if (typeof tree[key] === 'object') {
+                const subtree = generate_tree(tree[key], key, level.concat(key));
+                current_tree += subtree;
+            } else {
+                const levels = level.concat(key).join('.')
+                current_tree += `<div className='${key}'>{ ${levels} }</div>`;
+            }
+        });
+
+
+        return `<div className='${key}_container'>${current_tree}</div>`;
+    }
+
+    const make_tree = () => {
+        const [valid, json] = is_valid_json(input);
+
+        if (valid) {
+            const result = generate_tree(json, 'main', ['data']);
+            setOutput(result);
+
+        } else {
+            console.error('Wrong json', json);  
+        }
+    }
+
+    return (
+        <div className="container">
+            <textarea name="" value={input} onChange={(e) => change_text(e, FieldType.INPUT)}></textarea>
+            <textarea name="" value={output} onChange={(e) => change_text(e, FieldType.OUTPUT)}></textarea>
+            <button onClick={make_tree}>Convert</button>
+        </div>
+        )
 }
